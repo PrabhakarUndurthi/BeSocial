f37b8238206f08e4d77d7b7b1d41142e3ca69e0c
prabhakar <undurthi.prabhakar@gmail.com>
2014-08-14 13:09:15 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeSocial::Application.config.secret_key_base = '7f90fc139f971c9ec703eab99b508c893515dd7cc683078eaa32002a1974d55b851c764045c30f7093f381709282bbe94998c043d981bd51a03c3274a456648f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeSocial::Application.config.session_store :cookie_store, key: '_BeSocial_session'
